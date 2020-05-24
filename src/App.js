3be1af808754d7c94e2e94de195a913c5b712a14
Mic Lom <mlomzynski94@gmail.com>
2020-05-24 16:29:19 +0200

    change app js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    active: false,
+  }
+
+  handleBtn = () => {
+    this.setState({
+      active: !this.state.active,
+    })
+  }
+
+
+  render() {
+    let btn = {
+      border: '2px solid black',
+      padding: '10px 20px',
+      fontFamily: 'arial',
+      fontSize: '30px',
+      display: 'block',
+      margin: '20px auto',
+      backgroundColor: 'white',
+    }
+
+    if (this.state.active) {
+      btn.backgroundColor = 'cadetblue';
+      btn.color = 'white';
+    }
+
+    return (
+      <button style={btn} onClick={this.handleBtn}> {this.state.active ? 'Włącz' : 'Wyłącz'}</button >
+    );
+  }
 }
 
 export default App;
