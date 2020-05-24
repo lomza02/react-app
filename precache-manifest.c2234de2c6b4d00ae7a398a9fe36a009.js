4aa1f006cc0f912b5557895b05013f53dfd683b4
Mic Lom <mlomzynski94@gmail.com>
2020-05-24 16:53:47 +0200

    Updates

---
+++ precache-manifest.c2234de2c6b4d00ae7a398a9fe36a009.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "927f37ba27991769f0f05bdd5b66d30f",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "1c38347b3041c9eb51dd",
+    "url": "/react-app/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f4a85f52aeca6e83f883",
+    "url": "/react-app/static/js/2.aeb9f6b2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-app/static/js/2.aeb9f6b2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c38347b3041c9eb51dd",
+    "url": "/react-app/static/js/main.8778e4d6.chunk.js"
+  },
+  {
+    "revision": "5b4f537dcaf816bafbc5",
+    "url": "/react-app/static/js/runtime-main.2881a27a.js"
+  }
+]);
